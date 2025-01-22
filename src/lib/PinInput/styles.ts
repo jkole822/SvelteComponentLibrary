d3a5c66f16d0fc545ce43e017f6b7e1e3f87aa02
jkole822 <jkole822@gmail.com>
2025-01-22 07:06:41 -0500

    feature: PIN Input - Adjust ring styles

--- src/lib/PinInput/styles.ts
+++ src/lib/PinInput/styles.ts
@@ -3,14 +3,17 @@ import { oneLine } from "common-tags";
 export const ContainerStyles = oneLine`
 flex
 items-center
-gap-2
+gap-2.5
 pin-container
 `;
 
 export const InputStyles = oneLine`
-bg-neutral-700
+bg-neutral-900
+duration-300
+ease-in-out
 outline-none
 pin-input
+ring-2
 ring-neutral-600
 rounded-md
 shadow-sm
@@ -18,10 +21,12 @@ size-12
 text-center
 text-lg
 text-neutral-200
+transition-shadow
 
 disabled:cursor-not-allowed
 disabled:!opacity-25
 
-focus:ring-2
 focus:ring-primary-500
+
+hover:ring-neutral-400
 `;
